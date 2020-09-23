60e41e1671c9c1a1614e08c84a453acbf27d41e0
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-09-23 16:26:18 -0500

    week1

---
+++ week1/todopart1/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+      return (
+        <div>
+          <input type="checkbox" />
+          <p>Eat Breakfast</p>
+
+          <input type="checkbox" />
+          <p>Take Out The Trash</p>
+
+          <input type="checkbox" />
+          <p>Feed The Dog</p>
+
+          <input type="checkbox" />
+          <p>Shower</p>
+        </div>
+      )
+      }
+
+export default App
